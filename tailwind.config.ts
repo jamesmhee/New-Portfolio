39f953b1efce1c5420a59af834d286937e4a346f
jemmyblair <42910234+jamesmhee@users.noreply.github.com>
2024-08-23 22:15:37 +0700

    up

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
           "33%": { transform: "scale(1.2)" },
           "66%": { transform: "scale(0.8)" },
           "100%": { transform: "scale(1)" },
-        },
+        },                
       },
       animation: {                
         "pop-blob": "pop-blob 5s infinite",
